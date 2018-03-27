ca7a9e2a491b35271377c771c3e579c76095eb43
Dan Fiscus <dafiscus@gmail.com>
2018-03-27 01:53:42 -0400

    initial code commit

---
+++ main.cpp
@@ -0,0 +1,26 @@
+// this is the main file of execution
+
+#include <stdio.h>
+
+
+int main(){
+
+std::cout << "just use kleopatra fucker";
+
+
+return(0);
+}
+
+// encrypts message with some sort of cipher
+string encrypt(message2Encrypt){
+
+
+  return(encrypted);
+}
+
+// decrypts message
+string decrypt(message2Decrypt){
+
+
+  return(decrypted);
+}
