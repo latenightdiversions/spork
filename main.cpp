8a18b3c63fe820fd628346b6b37477b214b68d7e
Caelan Chapman <cchap.mkb@protonmail.com>
2018-03-27 03:30:09 -0400

    Framework of code, nothing useful.

--- main.cpp
+++ main.cpp
@@ -1,26 +1,91 @@
 // this is the main file of execution
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <string>
 
+std::string sendMessage(std::string);
+std::string message2Encrypt(std::string);
+std::string message2Encrypt(std::string);
+std::string encrypt(std::string);
 
-int main(){
+int main() {
 
-std::cout << "just use kleopatra fucker";
+    std::string messageSend; // message to be sent
+    std::string encryptedMessageSend; // encrypted message to be sent
+    std::string messageReceive; // message that is to be received
+    std::string encryptedMessageReceive; // message received and then deciphered
+    bool newMessage = false; // determines if there is incoming message
+    int selection; // selects options in main loop
 
+    do {
+        std::cout << "1.) Send Message\n";
+        std::cout << "2.) Receive Message\n";
+        std::cout << "3.) Quit\n";
+        std::cout << "Enter selection: ";
+        std::cin >> selection;
 
-return(0);
+        switch (selection) {
+        case 1:
+            //    std::cout << "Enter Message: ";
+            //    std::cin >> messageSend;
+            //    std::string encryptedMessageSend = encrypt(messageSend);
+            std::cout << "1\n";
+            break;
+
+        case 2:
+            std::cout << "2\n";
+            break;
+
+        case 3:
+            std::cout << "3\n";
+            break;
+
+        default:
+            std::cout << "You are a dumbass.\n";
+            break;
+        }
+
+    } while (selection != 3);
+
+    return (0);
 }
 
 // encrypts message with some sort of cipher
-string encrypt(message2Encrypt){
+std::string encrypt(std::string message2Encrypt) {
 
+    // some code
 
-  return(encrypted);
+    //return(encrypted);
 }
 
 // decrypts message
-string decrypt(message2Decrypt){
+std::string decrypt(std::string message2Decrypt) {
 
+    // some code
 
-  return(decrypted);
+    //return(decrypted);
 }
+
+// send encrypted message to peer
+std::string sendMessage(std::string encryptedMessage) {
+
+    // some code
+
+}
+
+/*
+TODO:
+- remote killswitch, both clients need to be activated
+- store everything in RAM, immediately destroyed upon closing client
+- message history for session
+- pictures...? open in system with something like feh
+- miltary encryption with... something? GPG maybe
+- ability to send forkbombs
+
+Interface:
+
+- basic colors for interface
+
+*/
